88320c88c3ee6fea51c8893d6cab3fc4a20eb17c
khoopeikoon <69444083+khoopeikoon@users.noreply.github.com>
2024-04-10 15:01:15 +0800

    Initial Commit

---
+++ Learning app 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning app 1
+//
+//  Created by Khoo Pei Koon on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning app 1/Learning_app_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_app_1App.swift
+//  Learning app 1
+//
+//  Created by Khoo Pei Koon on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Learning_app_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
