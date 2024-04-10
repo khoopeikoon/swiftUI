228c4360e0a5db43df240c9c761215b46c51deed
khoopeikoon <69444083+khoopeikoon@users.noreply.github.com>
2024-04-10 18:45:14 +0800

    Initial Commit Created CardView Created isFaceUp

--- Learning app 1/ContentView.swift
+++ Learning app 1/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+    }
+}
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content:{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 15).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth:5)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            
+                Text("🥲").font(.largeTitle)
+            } else {
+                RoundedRectangle (cornerRadius:15)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+        })
     }
 }
 
