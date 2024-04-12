ae88857b6726ae6a31d229405a26ccdb61cb7ec2
khoopeikoon <69444083+khoopeikoon@users.noreply.github.com>
2024-04-12 21:33:48 +0800

    Done with lesson 3

---
+++ Learning app 1/MemoryGameViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoryGameViewModel.swift
+//  Learning app 1
+//
+//  Created by Khoo Pei Koon on 12/4/24.
+//
+
+import SwiftUI
+
+class MemoryGameViewModel {
+    var model: memorizeGame<String>
+}
