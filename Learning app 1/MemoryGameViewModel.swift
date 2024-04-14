33f77455a7d827bd0fade2adfac13df515363238
khoopeikoon <69444083+khoopeikoon@users.noreply.github.com>
2024-04-14 16:30:32 +0800

    43 mins into lecture 4...

--- Learning app 1/MemoryGameViewModel.swift
+++ Learning app 1/MemoryGameViewModel.swift
@@ -8,5 +8,25 @@
 import SwiftUI
 
 class MemoryGameViewModel {
-    var model: memorizeGame<String>
+    private static let emojis = ["🥲","🤔","🙄","🤣","🥹","🥳","🤩"]
+
+    private var model = MemoryGameViewModel.createMemoryGame()
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 2) { pairIndex -> String in
+            if emojis.indices.contains(pairIndex) {
+                return MemoryGameViewModel.emojis[pairIndex]
+            } else {
+                return "🚫"
+            }
+        }
+    }
+    
+    var cards:Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+
+    func choose(_ card:MemoryGame<String>.Card){
+        model.choose(card)
+    }
 }
