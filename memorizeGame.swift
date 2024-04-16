2301f3acb16ec1d125f60ff75940a6674aed9ae9
khoopeikoon <69444083+khoopeikoon@users.noreply.github.com>
2024-04-16 18:13:45 +0800

    lecture 5 - identifiable and equatable

--- memorizeGame.swift
+++ memorizeGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -16,8 +16,8 @@ struct MemoryGame<CardContent> {
         
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
         }
     }
     
@@ -29,9 +29,12 @@ struct MemoryGame<CardContent> {
         cards.shuffle()
         print(cards)
     }
-    struct Card {
+    struct Card: Equatable, Identifiable {
         var isFaceUp = true
         var isMatched = false
         let content: CardContent
+        
+        var id: String
+        
     }
 }
